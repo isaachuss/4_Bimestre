4a7b35f0ee27b23ee31653d0b2c44a04beaec865
Isaac <ederaldohuss@gmail.com>
2025-11-07 11:56:21 -0300

    QUINTO COMMIT

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -15,10 +13,19 @@ function App() {
     setPassword(e.target.value);
   }
 
-  const enviarForms=(e)=>{
+  const enviarForms = (e) => {
     e.preventDefault()
     console.log(`Email: ${email}, Senha: ${senha}`)
+
+    
   }
+
+useEffect(() => {
+    fetch("https://www.api.alanleiser.com/")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.log('error'))
+}, []);
 
   return (
     <>
@@ -32,93 +39,117 @@ function App() {
           <label htmlFor="">Nome</label>
           <input type="text" placeholder='Nome' />
         </div>
-      
 
-      <div className='div_label'>
-        <label htmlFor="">Telefone</label>
-        <input type="number" placeholder='Telefone'  />
+
+        <div className='div_label'>
+          <label htmlFor="">Telefone</label>
+          <input type="number" placeholder='Telefone' />
+        </div>
+
+        <div className='div_label'>
+          <label htmlFor="">Idade</label>
+          <input type="date" placeholder='Idade' />
+        </div>
+
       </div>
 
-      <div className='div_label'>
-        <label htmlFor="">Idade</label>
-        <input type="date" placeholder='Idade' />
-      </div>
-      
-</div>
+      <div className='div_3'>
+        <div className='div_label'>
+          <label htmlFor="">Instagram</label>
+          <input type="text" placeholder='Instagram' />
+        </div>
 
-<div className='div_3'>
-      <div className='div_label'>
-        <label htmlFor="">Instagram</label>
-        <input type="text" placeholder='Instagram' />
-      </div>
+        <div className='div_label'>
+          <label htmlFor="">GitHub</label>
+          <input type="text" placeholder='GitHub' />
+        </div>
 
-      <div className='div_label'>
-        <label htmlFor="">GitHub</label>
-        <input type="text" placeholder='GitHub' />
-      </div>
-
-      <div className='div_label'>
-        <label htmlFor="">Pensamento</label>
-        <input type="text" placeholder='Pensamento' />
-      </div>
-</div>
-
-<div className='div_3'>
-      <div className='div_label'>
-        <label htmlFor="">Defeito</label>
-        <input type="text" placeholder='Defeito' />
+        <div className='div_label'>
+          <label htmlFor="">Pensamento</label>
+          <input type="text" placeholder='Pensamento' />
+        </div>
       </div>
 
-      <div className='div_label'>
-        <label htmlFor="">Ultima série assistida</label>
-        <input type="text" placeholder='Ultima série assistida' />
+      <div className='div_3'>
+        <div className='div_label'>
+          <label htmlFor="">Defeito</label>
+          <input type="text" placeholder='Defeito' />
+        </div>
+
+        <div className='div_label'>
+          <label htmlFor="">Ultima série assistida</label>
+          <input type="text" placeholder='Ultima série assistida' />
+        </div>
+
+        <div className='div_label'>
+          <label htmlFor="">Ultima filme assistida</label>
+          <input type="text" placeholder='Ultima filme assistida' />
+        </div>
       </div>
 
-      <div className='div_label'>
-        <label htmlFor="">Ultima filme assistida</label>
-        <input type="text" placeholder='Ultima filme assistida' />
-      </div>
-</div>
+      <div className='div_3'>
+        <div className='div_label'>
+          <label htmlFor="">Ultimo jogo jogado</label>
+          <input type="text" placeholder='Ultimo jogo jogado' />
+        </div>
 
-<div className='div_3'>
-      <div className='div_label'>
-        <label htmlFor="">Ultimo jogo jogado</label>
-        <input type="text" placeholder='Ultimo jogo jogado' />
-      </div>
+        <div className='div_label'>
+          <label htmlFor="">Música favorita</label>
+          <input type="text" placeholder='Música favorita' />
+        </div>
 
-      <div className='div_label'>
-        <label htmlFor="">Música favorita</label>
-        <input type="text" placeholder='Música favorita' />
-      </div>
-
-      <div className='div_label'>
-        <label htmlFor="">Genero</label>
-        <input type="text" placeholder='Genero' />
-      </div>
-</div>
-
-<div className='div_3'>
-      <div className='div_label'>
-        <label htmlFor="">Habilidade especial</label>
-        <input type="text" placeholder='Habilidade especial' />
+        <div className='div_label'>
+          <label htmlFor="">Genero</label>
+          <input type="text" placeholder='Genero' />
+        </div>
       </div>
 
-      <div className='div_label'>
-        <label htmlFor="">Poder especial</label>
-        <input type="text" placeholder='Poder especial' />
-      </div>
+      <div className='div_3'>
+        <div className='div_label'>
+          <label htmlFor="">Habilidade especial</label>
+          <input type="text" placeholder='Habilidade especial' />
+        </div>
 
-      <div className='div_label'>
-        <label htmlFor="">Time que torce</label>
-        <input type="text" placeholder='Time que torce' />
+        <div className='div_label'>
+          <label htmlFor="">Poder especial</label>
+          <input type="text" placeholder='Poder especial' />
+        </div>
+
+        <div className='div_label'>
+          <label htmlFor="">Time que torce</label>
+          <input type="text" placeholder='Time que torce' />
+
+
+
+        </div>
+
+       
+
       </div>
       <div>
-      <h1>CADASTRO</h1>
-      </div>
-      
-</div>
-      <div  >
-        <button>Enviar</button>
+        <form onSubmit={enviarForms}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => mudancaDeEmail(e)} />
+        </form>
+
+        <form onSubmit={enviarForms}>
+          <label htmlFor="password">Senha</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => mudancaDePassword(e)} />
+
+          <div className="register">
+            <button className="button" type="submite">Cadastrar</button>
+          </div>
+        </form>
       </div>
 
 
